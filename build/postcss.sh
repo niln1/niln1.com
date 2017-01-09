a5d6af7ae68fb1ffa6c42ff3046b9eeaf1e3907f
niln1 <nil@zhihaoni.com>
2017-01-09 10:16:25 -0800

    Add some style

---
+++ build/postcss.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Prefix and compress css into docs/
+postcss \
+  --use postcss-import  \
+  --use postcss-extend  \
+  --use autoprefixer    \
+  --use postcss-csso    \
+  --use postcss-url     \
+  ./src/style.css -d docs
