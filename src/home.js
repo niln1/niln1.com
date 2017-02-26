56e3d35587754f9a4077f882f7bb478603b8e3ad
niln1 <nil@zhihaoni.com>
2017-02-25 18:34:05 -0800

    Update data

--- src/home.js
+++ src/home.js
@@ -4,17 +4,44 @@ var gold = 1.61803
 var gold_inv = 0.61803
 var scale = 88
 
+var trend = 0
+var last = Math.random() * 50
+
+function getNext() {
+  if (trend === 0) { //向上
+    if (last<61) {
+      last = last+0.05
+      return last
+    } else {
+      trend = 1
+      last = last-0.05
+      return last
+    }
+  } else{ //向下
+    if (last>0) {
+      last = last-0.05
+      return last
+    } else {
+      trend = 0
+      last = last+0.05
+      return last
+    }
+  }
+}
+
+
 function render(seed) {
   if (window.innerWidth < 600) {
     scale = Math.min(window.innerWidth, window.innerHeight) / (gold^5)
   } else {
     scale = Math.min(window.innerWidth, window.innerHeight) / (gold^8)
   }
+  var next = getNext()
   var pattern = Trianglify({
       width: window.innerWidth,
       height: window.innerHeight,
-      cell_size: scale+Math.random()*scale*gold/10,
-      variance: Math.random()*.66*.88*.33,
+      cell_size: scale,
+      variance: next,
       seed: 'niln1'
   });
 
@@ -22,7 +49,8 @@ function render(seed) {
 }
 
 render()
-var time = 1618.03
-setInterval(render, time)
-setTimeout(setInterval(render, time/gold), time/gold);
-setTimeout(setInterval(render, time/gold_inv), time/gold_inv);
+
+var time = 200.03
+// setInterval(render, time)
+setTimeout(function() {return setInterval(render, time/gold)}, 10000);
+setTimeout(function() {return setInterval(render, time/gold_inv)}, 13000);
